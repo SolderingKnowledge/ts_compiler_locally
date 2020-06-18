cc623fecf61bc268849401523db2d2034642474e
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-06-17 17:51:00 -0700

    class

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 "use strict";
-const addBorder = (picture) => {
-    const wall = "*".repeat(picture[0].length + 2); //
-    picture.unshift(wall);
-    picture.push(wall);
-    for (let i = 0; i < picture.length - 1; i++) {
-        picture[i] = "*".concat(picture[i], "*");
+// const addBorder=(picture:string[]): string[] => {
+//     const wall= "*".repeat(picture[0].length+2); //
+//     picture.unshift(wall);
+//     picture.push(wall);
+//     for(let i=0; i<picture.length-1; i++){
+//         picture[i]="*".concat(picture[i], "*");
+//     }
+//     return picture;
+// }
+// console.log(addBorder(["abc", "bbc"]));//["*******", "*abc*", "*bbc*", "*****"]
+class Some {
+    constructor(eyes) {
+        this.me = "red"; //me = "red";
+        this.eyes = eyes;
     }
-    return picture;
-};
-console.log(addBorder(["abc", "bbc"])); //["*******", "*abc*", "*bbc*", "*****"]
+}
+var some = new Some(2);
+console.log(some.eyes, some.me); // 2 "red"
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,11 +1,25 @@
-const addBorder=(picture:string[]):any => {
-    const wall= "*".repeat(picture[0].length+2); //
-    picture.unshift(wall);
-    picture.push(wall);
-    for(let i=0; i<picture.length-1; i++){
-        picture[i]="*".concat(picture[i], "*");
+// const addBorder=(picture:string[]): string[] => {
+//     const wall= "*".repeat(picture[0].length+2); //
+//     picture.unshift(wall);
+//     picture.push(wall);
+//     for(let i=0; i<picture.length-1; i++){
+//         picture[i]="*".concat(picture[i], "*");
+//     }
+//     return picture;
+// }
+// console.log(addBorder(["abc", "bbc"]));//["*******", "*abc*", "*bbc*", "*****"]
+
+class Some {
+    me: string = "red"; //me = "red";
+    eyes: number;
+    constructor(eyes: number){
+      this.eyes = eyes;
     }
-    return picture;
-}
-console.log(addBorder(["abc", "bbc"]));//["*******", "*abc*", "*bbc*", "*****"]
+  }
+  
+var some = new Some(2);
+console.log(some.eyes, some.me);// 2 "red"
+
+
+
 

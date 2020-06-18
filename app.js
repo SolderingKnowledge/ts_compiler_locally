ff852bd3c1a20d2d874bb8b602ef52b06118487a
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-06-17 17:33:04 -0700

    changed some file

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("hey");
+const addBorder = (picture) => {
+    const wall = "*".repeat(picture[0].length + 2); //
+    picture.unshift(wall);
+    picture.push(wall);
+    for (let i = 0; i < picture.length - 1; i++) {
+        picture[i] = "*".concat(picture[i], "*");
+    }
+    return picture;
+};
+console.log(addBorder(["abc", "bbc"])); //["*******", "*abc*", "*bbc*", "*****"]
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log("hey");
+const addBorder=(picture:string[]):any => {
+    const wall= "*".repeat(picture[0].length+2); //
+    picture.unshift(wall);
+    picture.push(wall);
+    for(let i=0; i<picture.length-1; i++){
+        picture[i]="*".concat(picture[i], "*");
+    }
+    return picture;
+}
+console.log(addBorder(["abc", "bbc"]));//["*******", "*abc*", "*bbc*", "*****"]
+
